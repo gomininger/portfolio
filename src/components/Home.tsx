89a362a04bd1f434ff57e681f4f1dfc6d69a6272
gomininger <gomininger@gmail.com>
2025-07-09 19:39:01 +0700

    Fix Home component syntax error

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,1 +1,12 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio!</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+};
+
+export default Home;
